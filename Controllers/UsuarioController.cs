81cd4fd9a9a4c912998c5fde44e21c33901037e4
joaopauloclass <theofficialspam8@gmail.com>
2025-04-06 20:22:34 -0300

    Usuario CRUD Finalization

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -33,6 +33,24 @@ namespace ControlePlus_BackEnd.Controllers
 
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuarioById(int id)
+        {
+            try
+            {
+                var usuario = await _database.tb_usuario.FirstOrDefaultAsync(u => u.Id == id);
+                if (usuario != null){
+                    return Ok(usuario);
+                }
+
+                return NotFound($"Usuario id {id} não encontrado");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500, "Erro ao buscar dados");
+            }
+        }
         
 
         [HttpPost]
@@ -48,6 +66,9 @@ namespace ControlePlus_BackEnd.Controllers
                 var validacao = await _database.tb_usuario.FirstOrDefaultAsync(u => u.Id == usuario.Id);
                 if (validacao == null)
                 {
+
+                    usuario.DataCriacao = DateTime.UtcNow;
+                    usuario.DataUltimaAtualizacao = DateTime.UtcNow;
                     _database.tb_usuario.Add(usuario);
                     await _database.SaveChangesAsync();
                     // return CreatedAtAction(nameof(GetUsuarioById), new { nome = usuario.Nome }, usuario );
@@ -59,6 +80,68 @@ namespace ControlePlus_BackEnd.Controllers
             {
                 Console.WriteLine(ex);
                 return StatusCode(500, "Erro ao adicionar novo usuario");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> ModifyUser(int id, [FromBody] Usuario usuarioMod)
+        {
+            try
+            {
+                // var usuario = await _database.tb_usuario.FindAsync(id);
+
+                // if (usuario == null)
+                // {
+                //     return NotFound($"usuario id {id} não encontrado");
+                // }
+
+                // _database.Entry(usuario).CurrentValues.SetValues(usuarioMod);
+
+                // await _database.SaveChangesAsync();
+                // return NoContent();
+                var usuarioExistente = await _database.tb_usuario.FindAsync(id);
+                if (usuarioExistente != null)
+                {
+                    usuarioExistente.Nome = usuarioMod.Nome;
+                    usuarioExistente.Username = usuarioMod.Username;
+                    usuarioExistente.SetorId = usuarioMod.SetorId;
+                    usuarioExistente.Roles = usuarioMod.Roles;
+                    usuarioExistente.Ativo = usuarioMod.Ativo;
+                    usuarioExistente.DataUltimaAtualizacao = DateTime.UtcNow;
+
+                    await _database.SaveChangesAsync();
+                    return Ok(usuarioExistente);
+                }
+                return NotFound($"Setor id {id} não encontrado");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500, "Erro ao modificar o Usuario");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsuario(int id)
+        {
+
+            try
+            {
+                var usuario = await _database.tb_usuario.FirstOrDefaultAsync(u => u.Id == id);
+
+                if (usuario != null)
+                {
+                    _database.Remove(usuario);
+                    await _database.SaveChangesAsync();
+                    return Ok();
+                }
+
+                return NotFound($"Não fpo possível encontrar o usuario com id {id}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500, "Erro ao deletar usuario");
             }
         }
 
