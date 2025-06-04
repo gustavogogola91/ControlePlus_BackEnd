79bed98e16cf160cbd85d35e32179ed50faddabc
joaopauloclass <theofficialspam8@gmail.com>
2025-06-04 16:04:55 -0300

    Mostrar pedidos em alerta dashboard

---
+++ Frontend/controleplus-front/src/app/pages/home/actions.ts
@@ -0,0 +1,18 @@
+const ApiUrlPedido = "http://localhost:5290/pedido";
+const ApiUrlEstoque = "http://localhost:5290/estoque";
+
+
+
+//APIs ESTOQUE
+
+export async function buscarEstoqueAlerta() {
+    const response = await fetch(`${ApiUrlEstoque}/alerta`);
+    if (!response.ok) throw new Error("Erro ao carregar produtos");
+    return response.json();
+}
+
+export async function buscarEstoqueVazio() {
+    const response = await fetch(`${ApiUrlEstoque}/vazio`);
+    if (!response.ok) throw new Error("Erro ao carregar produtos");
+    return response.json();
+}
--- Frontend/controleplus-front/src/app/pages/home/dashboard.tsx
+++ Frontend/controleplus-front/src/app/pages/home/dashboard.tsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
+import { buscarEstoqueAlerta, buscarEstoqueVazio } from "./actions";
 
 
 const dashboard = () => {
+
+    const [estoquesAlerta, setEstoquesAlerta] = useState([]);
+    const [estoquesVazio, setEstoquesVazio] = useState([]);
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+    
+        async function carregarEstoques() {
+          try {
+            let estoqueAlerta: any = null;
+            let estoqueVazio: any = null;
+            estoqueAlerta = await buscarEstoqueAlerta();
+            estoqueVazio = await buscarEstoqueVazio();
+            
+            setEstoquesAlerta(estoqueAlerta);
+            setEstoquesVazio(estoqueVazio);
+            setLoading(false);
+          } catch (err) {
+            console.log(err instanceof Error ? err.message : "Erro desconhecido");
+            setLoading(false);
+            setEstoquesAlerta([]);
+            setEstoquesVazio([]);
+          }
+    
+    
+        }
+    
+        carregarEstoques();
+      }, []);
 
 
     return (
@@ -8,13 +40,14 @@ const dashboard = () => {
             <section className="grid gap-2 grid-cols-3">
                 <div className="flex flex-col justify-center w-[350px] h-[170px] shadow border-gray rounded px-5">
                     <p className="font-semibold text-[16px]">Produtos em alerta</p>
-                    <p className="font-semibold text-[40px] text-orange">2</p>
-                    <p className="font-medium text-[16px] text-gray">Nenhum pedido feito</p>
+                    <p className="font-semibold text-[40px] text-orange">{estoquesAlerta.length}</p>
+                    {/* <p className="font-medium text-[16px] text-gray">Nenhum pedido feito</p> //TODO se der tempo implementar isso*/}
                 </div>
                 <div className="flex flex-col justify-center w-[350px] h-[170px] shadow border-gray rounded px-5">
                     <p className="font-semibold text-[16px]">Produtos em falta</p>
-                    <p className="font-semibold text-[40px] text-red">4 <span className="text-[24px]">  &#x276f; 2</span></p>
-                    <p className="font-medium text-[16px] text-gray"><span className="text-blue">4 </span>produtos com pedidos criados</p>
+                    <p className="font-semibold text-[40px] text-red">{estoquesVazio.length}</p>
+                    {/* <span className="text-[24px]">  &#x276f; 2</span> */}
+                    {/* <p className="font-medium text-[16px] text-gray"><span className="text-blue">4 </span>produtos com pedidos criados</p> //TODO se der tempo implementar isso*/}
                 </div>
                 <div className="flex flex-col justify-evenly w-[350px] h-[170px] shadow border-gray rounded px-5">
                     <p className="font-semibold text-[16px]">Ações</p>
